2a28eece3842c001c71bda55fb19f5aca668cd21
Alba García <agartru2003@g.educaand.es>
2024-11-05 10:48:41 +0100

    Commit numero 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.security.SecureRandom;
 
 public class Main {
     public static void main(String[]args){
-        System.out.println("Hola, mundo!");
+        System.out.println("Hola, mundo! Esta es una nueva\n" +
+                "funcionalidad.");
 
     }
 }
